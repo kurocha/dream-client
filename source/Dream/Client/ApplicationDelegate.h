7e038be5b8f17d32e38f5345cff39cb632d7235d
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2013-09-11 01:56:29 +1200

    Fixed incorrect declaration.

--- source/Dream/Client/ApplicationDelegate.h
+++ source/Dream/Client/ApplicationDelegate.h
@@ -34,8 +34,6 @@ namespace Dream
 
 			virtual void application_will_enter_background (IApplication * application);
 			virtual void application_did_enter_foreground (IApplication * application);
-			
-			static Ref<Resources::ILoader> default_resource_loader ();
 		};
 		
 		Ref<Resources::ILoader> default_resource_loader ();
