949de280b1db087885b7e09d703be3f6c4d5ccdf
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2014-10-15 17:16:48 +1300

    Update for teapot 1.0

--- teapot.rb
+++ teapot.rb
@@ -3,12 +3,21 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-teapot_version "0.8.0"
+teapot_version "1.0"
 
 define_target "dream-client" do |target|
-	target.build do |environment|
-		build_directory(package.path, 'source', environment)
+	target.build do
+		source_root = target.package.path + 'source'
+		
+		copy headers: source_root.glob('Dream/**/*.{h,hpp}')
+		
+		build static_library: "DreamClient", source_files: source_root.glob('Dream/**/*.cpp')
 	end
+	
+	target.depends :platform
+	target.depends "Build/Files"
+	target.depends "Build/Clang"
+	target.depends "Language/C++11"
 	
 	target.depends "Library/Dream"
 	
@@ -61,5 +70,4 @@ define_configuration "travis" do |configuration|
 	
 	configuration.require "platforms"
 	configuration.require "dream-display-osx"
-	configuration.import "dream-client"
 end
